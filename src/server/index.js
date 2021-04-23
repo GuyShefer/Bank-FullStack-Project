4871d91940531768db2fab288857beb6e1b46c7f
Guy <shefer.guy1@gmail.com>
2021-04-24 00:37:05 +0300

    .

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,10 @@
 const app = require('./server');
-const port = 8000;
+const port = 5000;
 
 app.get('/',(req,res)=>{
     res.json({success : 'Bank API'})
 })
 
-app.listen(process.env.PORT || port, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`application start at ${port}`);
 })
