9ca6b174c3841b5180c03e5b54cf005384b109f6
Guy <shefer.guy1@gmail.com>
2021-04-24 00:20:33 +0300

    gilad fix

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,10 @@
 const app = require('./server');
 const port = 8000;
 
+app.get('/',(req,res)=>{
+    res.json({success : 'Bank API'})
+})
+
 app.listen(process.env.PORT || port, () => {
     console.log(`application start at ${port}`);
 })
