94e254ebae5a15a74f58e78d92cf145b0c44bdf1
Guy <shefer.guy1@gmail.com>
2021-04-25 00:36:52 +0300

    .

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ const app = express();
 const userRoutes = require('./src/routes/users.routes');
 const cors = require('cors');
 require('./src/data/mongoose');
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'build')))
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/api/bank', userRoutes);
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 module.exports = app;
